cda45504d55351469b2031b3ebb02b93ecd205c9
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 04:54:36 +0800

    Show Home overlay immediately on navigation

--- input/s1_input_dispatch.h
+++ input/s1_input_dispatch.h
@@ -4,12 +4,23 @@
 #include "../ui/s1_ui.h"
 #include "../ui/ui_router.h"
 #include "../ui/pages/ui_led.h"
+#include "../ui/pages/ui_home_overlay.h"
 
 
 /*
  * Central input routing shared by the W1 remote. Music and LED are HOME branch
  * pages: they consume navigation locally and only BACK/HOME leave the branch.
  */
+static inline void s1_input_sync_home_overlay(void)
+{
+    if(s1_ui_router_current() == S1_PAGE_HOME) {
+        s1_ui_home_overlay_show();
+    }
+    else {
+        s1_ui_home_overlay_hide();
+    }
+}
+
 static inline void s1_input_dispatch_key(uint32_t key)
 {
     s1_page_id_t before = s1_ui_router_current();
@@ -17,10 +28,12 @@ static inline void s1_input_dispatch_key(uint32_t key)
     if(before == S1_PAGE_MUSIC) {
         if(key == LV_KEY_UP || key == LV_KEY_ESC || key == LV_KEY_HOME) {
             s1_ui_key(LV_KEY_HOME);
+            s1_input_sync_home_overlay();
             return;
         }
 
         s1_ui_key(key);
+        s1_input_sync_home_overlay();
         return;
     }
 
@@ -28,6 +41,7 @@ static inline void s1_input_dispatch_key(uint32_t key)
         if(key == LV_KEY_ESC || key == LV_KEY_HOME) {
             s1_ui_led_hide();
             s1_ui_key(LV_KEY_HOME);
+            s1_input_sync_home_overlay();
             return;
         }
 
@@ -38,10 +52,12 @@ static inline void s1_input_dispatch_key(uint32_t key)
     s1_ui_key(key);
 
     if(s1_ui_router_current() == S1_PAGE_LED) {
+        s1_ui_home_overlay_hide();
         s1_ui_led_show();
     }
     else {
         s1_ui_led_hide();
+        s1_input_sync_home_overlay();
     }
 }
 
