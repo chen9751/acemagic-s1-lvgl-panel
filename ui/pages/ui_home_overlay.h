70567beedf6e31acbe920189a18e054b3206a017
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 04:53:45 +0800

    Expose immediate Home overlay visibility

--- ui/pages/ui_home_overlay.h
+++ ui/pages/ui_home_overlay.h
@@ -1,7 +1,11 @@
 #ifndef S1_UI_HOME_OVERLAY_H
 #define S1_UI_HOME_OVERLAY_H
 
+#include <stdbool.h>
+
 void s1_ui_home_overlay_init(void);
+void s1_ui_home_overlay_show(void);
+void s1_ui_home_overlay_hide(void);
 void s1_ui_home_overlay_set_weather(int temperature_c, int rain_probability_percent, int weather_code);
 
 #endif
