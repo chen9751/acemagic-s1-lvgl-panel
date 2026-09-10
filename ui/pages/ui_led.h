e5e50e9b66d28a15b6862464781ed237433e6f44
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 04:23:14 +0800

    Add dedicated LED control page header

---
+++ ui/pages/ui_led.h
@@ -0,0 +1,11 @@
+#ifndef UI_LED_H
+#define UI_LED_H
+
+#include "lvgl/lvgl.h"
+
+void s1_ui_led_init(void);
+void s1_ui_led_show(void);
+void s1_ui_led_hide(void);
+void s1_ui_led_key(uint32_t key);
+
+#endif
