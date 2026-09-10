bbffde2c42b366bf6deb652273f1d09f93e43439
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 02:55:55 +0800

    Add Home Assistant light state sync API

--- services/ha_client.h
+++ services/ha_client.h
@@ -1,12 +1,25 @@
 #ifndef HA_CLIENT_H
 #define HA_CLIENT_H
 
+typedef struct {
+    int is_on;
+    int brightness_percent;
+    int color_temperature_kelvin;
+    int has_brightness;
+    int has_color_temperature;
+} ha_light_state_t;
+
 int ha_client_init(void);
 
 int ha_get_state(
     const char *entity_id,
     char *state_buf,
     int state_buf_size
+);
+
+int ha_get_light_state(
+    const char *entity_id,
+    ha_light_state_t *state
 );
 
 int ha_set_light_power(const char *entity_id, int on);
