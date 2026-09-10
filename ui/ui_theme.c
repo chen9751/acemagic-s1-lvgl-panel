8c5584da216d2ad0ebb07829f0abf4b6d194270b
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 04:54:48 +0800

    Strengthen shared S1 gradient background

--- ui/ui_theme.c
+++ ui/ui_theme.c
@@ -1,8 +1,8 @@
 #include "ui_theme.h"
 #include "lvgl/lvgl.h"
 
-#define THEME_BG_TOP    0x080A0F
-#define THEME_BG_BOTTOM 0x0B1822
+#define THEME_BG_TOP    0x070A10
+#define THEME_BG_BOTTOM 0x102A38
 
 static void apply_to_large_top_level(lv_obj_t *obj)
 {
@@ -11,12 +11,23 @@ static void apply_to_large_top_level(lv_obj_t *obj)
         lv_obj_set_style_bg_color(obj, lv_color_hex(THEME_BG_TOP), 0);
         lv_obj_set_style_bg_grad_color(obj, lv_color_hex(THEME_BG_BOTTOM), 0);
         lv_obj_set_style_bg_grad_dir(obj, LV_GRAD_DIR_VER, 0);
+        lv_obj_set_style_bg_main_stop(obj, 0, 0);
+        lv_obj_set_style_bg_grad_stop(obj, 255, 0);
+        lv_obj_set_style_bg_opa(obj, LV_OPA_COVER, 0);
     }
 }
 
 void s1_ui_apply_gradient_theme(void)
 {
     lv_obj_t *screen = lv_screen_active();
+
+    lv_obj_set_style_bg_color(screen, lv_color_hex(THEME_BG_TOP), 0);
+    lv_obj_set_style_bg_grad_color(screen, lv_color_hex(THEME_BG_BOTTOM), 0);
+    lv_obj_set_style_bg_grad_dir(screen, LV_GRAD_DIR_VER, 0);
+    lv_obj_set_style_bg_main_stop(screen, 0, 0);
+    lv_obj_set_style_bg_grad_stop(screen, 255, 0);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
+
     uint32_t count = lv_obj_get_child_count(screen);
     for(uint32_t i = 0; i < count; i++) {
         apply_to_large_top_level(lv_obj_get_child(screen, (int32_t)i));
