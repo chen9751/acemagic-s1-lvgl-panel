508195be3e28547c98a721c1c692d1d6712f783d
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-11 04:45:37 +0800

    Add Home overlay interface

---
+++ ui/pages/ui_home_overlay.h
@@ -0,0 +1,7 @@
+#ifndef S1_UI_HOME_OVERLAY_H
+#define S1_UI_HOME_OVERLAY_H
+
+void s1_ui_home_overlay_init(void);
+void s1_ui_home_overlay_set_weather(int temperature_c, int rain_probability_percent, int weather_code);
+
+#endif
