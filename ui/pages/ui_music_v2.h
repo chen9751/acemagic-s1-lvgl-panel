e6f83b04f90d90fbc5b600de86eac3f00641e847
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-14 01:18:24 +0800

    Add redesigned music status overlay

---
+++ ui/pages/ui_music_v2.h
@@ -0,0 +1,12 @@
+#ifndef UI_MUSIC_V2_H
+#define UI_MUSIC_V2_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void s1_ui_music_v2_init(void);
+void s1_ui_music_v2_set_state(bool connected, bool playing);
+void s1_ui_music_v2_set_progress(uint32_t elapsed_seconds, uint32_t duration_seconds);
+void s1_ui_music_v2_key_feedback(uint32_t key);
+
+#endif
