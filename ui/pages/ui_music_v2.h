5d8c0a6ec9e997b14aed3a51baa02ba5d38ea8d6
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-14 01:24:43 +0800

    Expose music overlay visibility sync

--- ui/pages/ui_music_v2.h
+++ ui/pages/ui_music_v2.h
@@ -8,5 +8,6 @@ void s1_ui_music_v2_init(void);
 void s1_ui_music_v2_set_state(bool connected, bool playing);
 void s1_ui_music_v2_set_progress(uint32_t elapsed_seconds, uint32_t duration_seconds);
 void s1_ui_music_v2_key_feedback(uint32_t key);
+void s1_ui_music_v2_sync_visibility(void);
 
 #endif
